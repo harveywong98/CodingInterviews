34e2b52e60d11cbbfc9c885ac3760e0ddeaa17f5
Harvey Wong <im.harveywong@outlook.com>
2021-08-22 19:40:04 +0800

    完成25：合并两个排序的链表(优化)

--- src/com/hello/_25/_25.java
+++ src/com/hello/_25/_25.java
@@ -8,47 +8,24 @@ public class _25 {
 
 class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if (null == l1){
-            return l2;
-        }
-        if (null == l2){
-            return l1;
-        }
 
-        ListNode p1;
-        ListNode p2;
-        ListNode cur;
-        ListNode result;
+        // 创建伪头节点
+        ListNode dum = new ListNode(0);
+        ListNode cur = dum;
 
-        if (l1.val <= l2.val){
-            p1 = l1.next;
-            p2 = l2;
-            cur = l1;
-            result = l1;
-        } else {
-            p1 = l1;
-            p2 = l2.next;
-            cur = l2;
-            result = l2;
-        }
-
-        while (null != p1 && null != p2){
-            if (p1.val <= p2.val){
-                cur.next = p1;
-                p1 = p1.next;
+        while (null != l1 && null != l2){
+            if (l1.val <= l2.val){
+                cur.next = l1;
+                l1 = l1.next;
             } else {
-                cur.next = p2;
-                p2 = p2.next;
+                cur.next = l2;
+                l2 = l2.next;
             }
             cur = cur.next;
         }
-        if (null == p1){
-            cur.next = p2;
-        } else if (null == p2){
-            cur.next = p1;
-        }
 
-        return result;
+        cur.next = null == l1 ? l2 : l1;
+        return dum.next;
     }
 }
 
