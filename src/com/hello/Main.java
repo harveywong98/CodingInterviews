09d1ca671ac852741b37b9f14ef6c50885817731
Harvey Wong <im.harveywong@outlook.com>
2021-08-23 23:21:14 +0800

    21:添加第二种解法

--- src/com/hello/Main.java
+++ src/com/hello/Main.java
@@ -4,5 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        int a = 5;
+        printX(a++);
+        System.out.println(a);
+    }
+
+    public static void printX(int x){
+        System.out.println(x);
     }
 }
--- src/com/hello/_21/_21.java
+++ src/com/hello/_21/_21.java
@@ -3,6 +3,11 @@ package com.hello._21;
 public class _21 {
     public static void main(String[] args) {
         // 调整数组顺序使奇数位于偶数前面
+        System.out.println(2&1);
+        System.out.println(123&1);
+        System.out.println(124&1);
+        Solution2 solution = new Solution2();
+        System.out.println(solution.exchange(new int[]{1,3,5,7,2,6,9}));
     }
 }
 
@@ -30,6 +35,22 @@ class Solution {
             int t = nums[left];
             nums[left] = nums[right];
             nums[right] = t;
+        }
+        return nums;
+    }
+}
+
+class Solution2{
+    public int[] exchange(int[] nums) {
+        int slow = 0,fast = 0;
+        while(fast<nums.length){
+            if ((nums[fast] & 1) == 1){
+                int t = nums[slow];
+                nums[slow] = nums[fast];
+                nums[fast] = t;
+                slow ++;
+            }
+            fast ++;
         }
         return nums;
     }
