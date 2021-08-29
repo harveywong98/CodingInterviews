65c492b8651c40f965880eb47655a55f1612d0bc
Harvey Wong <im.harveywong@outlook.com>
2021-08-29 23:29:54 +0800

    完成68 - I. 二叉搜索树的最近公共祖先

---
+++ src/com/hello/_68_1/_68_1.java
@@ -0,0 +1,34 @@
+package com.hello._68_1;
+
+public class _68_1 {
+    public static void main(String[] args) {
+        // 68 - I. 二叉搜索树的最近公共祖先
+    }
+}
+
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        TreeNode ancestor = root;
+        while (true){
+            if (ancestor.val < p.val && ancestor.val < q.val){
+                ancestor = ancestor.left;
+            } else if(ancestor.val > p.val && ancestor.val >q.val){
+                ancestor = ancestor.right;
+            } else {
+                break;
+            }
+        }
+        return ancestor;
+    }
+}
+
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
