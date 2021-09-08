e36be4b054608ebef8c918644c5545285ffaff4d
Harvey Wong <im.harveywong@outlook.com>
2021-09-08 20:53:39 +0800

    学习60. n个骰子的点数

---
+++ src/com/hello/_60/_60.java
@@ -0,0 +1,26 @@
+package com.hello._60;
+
+import java.util.Arrays;
+
+public class _60 {
+    public static void main(String[] args) {
+        // 60. n个骰子的点数
+    }
+}
+
+class Solution {
+    public double[] dicesProbability(int n) {
+        double[] dp = new double[6];
+        Arrays.fill(dp, 1.0 / 6.0);
+        for (int i = 2; i <= n; i++) {
+            double[] temp = new double[5 * i + 1];
+            for (int j = 0; j < dp.length; j++) {
+                for (int k = 0; k < 6; k++) {
+                    temp[j + k] += dp[j] * (1.0 / 6.0);
+                }
+            }
+            dp = temp;
+        }
+        return dp;
+    }
+}
